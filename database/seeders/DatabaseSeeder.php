b365429a0e4958063a35716b7caa3a0d72424459
Victor Valencia <sosviperdigital@gmail.com>
2024-05-16 23:25:04 -0500

    prep for fly launch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'description' => "Ingenieros S.R.L.",
-            'profile_photo_path' => "https://bkportafolio.test/profile-photos/LOGO.jpg",
+            'profile_photo_path' => "https://bkportafolio.test/images/LOGO.jpg",
             'facebook_url' => "#",
             'twitter_url' => "#",
             'dribbble_url' => "#",
